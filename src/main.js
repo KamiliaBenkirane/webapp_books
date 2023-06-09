c5dc1e22357796be181c1bca53a4fa31f41e602f
Marina ANASSOU <bomart.anassou@efrei.net>
2023-06-09 19:34:25 +0200

    frontend home page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/router.js'
+import { createApp } from 'vue';
+import App from './App.vue';
+import Home from './components/Home.vue';
+import router from './router/router.js';
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+// eslint-disable-next-line vue/multi-word-component-names
+app.component('home', Home);
+
+app.use(router);
+
+app.mount('#app');
